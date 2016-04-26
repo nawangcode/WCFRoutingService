f0300358dd892fc51ffb0b806d4a694748a79e4c
nawangcode <na.wang@rauland.com>
2016-04-26 16:38:54 -0500

    WCF routing service
    created in VS 2010

---
+++ DemoClient/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+using System.Threading;
+namespace DemoClient
+{
+    class Program
+    {
+  //http://www.codeproject.com/Articles/566691/WCF-From-a-Beginners-perspective-a-Tutorial
+        //http://www.codeproject.com/Articles/772854/WCF-Routing-Service-Part-I-Basic-Concept-Simple-Ro
+      static void Main(string[] args)
+        {
+
+            //RequestReplyRouterClient client = new RequestReplyRouterClient("BasicHttpBinding_IService");
+            //var c = client.ChannelFactory.CreateChannel();
+            Console.WriteLine("hello client");
+          
+            var cf = new ChannelFactory<IService>("BasicHttpBinding_IService");
+            var channel = cf.CreateChannel();
+            int i = 1;
+            //while (true)
+            {
+                Console.WriteLine("\n\n Reply from Server :" + channel.GetData(i++));
+                Console.WriteLine("\n\n------------------------------------------------------");
+              //  Thread.Sleep(1000);
+            }
+
+            var cf2 = new ChannelFactory<IService>("BasicHttpBinding_IService2");
+            var channel2 = cf2.CreateChannel();
+            int i2 = 11;
+            //while (true)
+            {
+                Console.WriteLine("\n\n Reply from Server :" + channel2.GetData(i2++));
+                Console.WriteLine("\n\n------------------------------------------------------");
+                Thread.Sleep(1000);
+            }
+
+            var cf3 = new ChannelFactory<IService>("BasicHttpBinding_IService3");
+            var channel3 = cf3.CreateChannel();
+          //channel3.
+            //int i2 = 11;
+            ////while (true)
+            //{
+            //    Console.WriteLine("\n\n Reply from Server :" + channel2.GetData(i2++));
+            //    Console.WriteLine("\n\n------------------------------------------------------");
+            //    Thread.Sleep(1000);
+            //}
+            //ServiceClient client = new ServiceClient();
+
+            //// Use the 'client' variable to call operations on the service.
+            //Console.WriteLine(client.GetData(10));
+
+            //CompositeType type = new CompositeType();
+            //type.BoolValue = true;
+            //type.StringValue = "NaW";
+
+            //CompositeType var = client.GetDataUsingDataContract(type);
+            //Console.WriteLine("1: " + var.BoolValue + " 2: " + var.StringValue);
+            //// Always close the client.
+            Console.ReadLine();
+            ((IClientChannel)channel).Close();
+            ((IClientChannel)channel2).Close();
+        }
+    }
+}
---
+++ WcfRoutingWindowsService/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.ServiceModel.Dispatcher;
+using System.ServiceModel.Routing;
+
+namespace WcfRoutingWindowsService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //SERVICE HOST
+            ServiceHost host = new ServiceHost(typeof(RoutingService));
+  //          foreach (var sb in host.Description.Behaviors)
+  //          {
+  //              ServiceDebugBehavior sdb = sb as ServiceDebugBehavior;
+  //              if (sdb != null)
+  //              {
+  //                  sdb.IncludeExceptionDetailInFaults = true;
+  //              }
+  //          }
+
+  //          //SERVICE END POINT - three endpoints: the service host (the router) and the two services to which it routes requests
+  //          ContractDescription cdRouter = ContractDescription.GetContract(typeof(IRequestReplyRouter));
+
+  //          // ROUTER'S SERVICE END POINT
+  //          ServiceEndpoint se = new ServiceEndpoint(cdRouter,
+  //new WSHttpBinding(),
+  //new EndpointAddress(
+  //  "http://localhost:9000/CustomerService/"));
+  //          host.AddServiceEndpoint(se);
+
+  //          ServiceEndpoint MainService = new ServiceEndpoint(cdRouter,
+  //            new WSHttpBinding(),
+  //            new EndpointAddress(
+  //              "http://localhost:50281/WCFService/Service.svc"));
+
+
+  //          MessageFilterTable<IEnumerable<ServiceEndpoint>> mft =
+  //new MessageFilterTable<IEnumerable<ServiceEndpoint>>();
+  //          RoutingConfiguration rc = new RoutingConfiguration(mft, false);
+  //          RoutingBehavior rb = new RoutingBehavior(rc);
+  //          host.Description.Behaviors.Add(rb);
+
+  //          List<ServiceEndpoint> primaryEndPoints = new List<ServiceEndpoint>();
+  //          primaryEndPoints.Add(MainService);
+  //          MessageFilter mamf = new MatchAllMessageFilter();
+  //          mft.Add(mamf, primaryEndPoints);
+
+            host.Open();
+            Console.WriteLine("Router up");
+            Console.ReadLine();
+            host.Close();
+        }
+    }
+}
